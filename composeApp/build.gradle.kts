d2a94f2bcd66efee05fcd05b78bcadcd84f52bdb
HyunWoo Lee <l2hyunwoo@gmail.com>
2026-03-09 01:15:37 +0900

    chore: add kotlinx-serialization, navigation3

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
@@ -20,6 +21,10 @@ kotlin {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.jetbrains.material3.adaptiveNavigation3)
+            implementation(libs.jetbrains.lifecycle.viewmodelNavigation3)
+            implementation(libs.compose.icons)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
